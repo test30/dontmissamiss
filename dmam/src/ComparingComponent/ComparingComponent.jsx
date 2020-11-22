f145e7f60b6341b9b4ff5decc276168701a00ce7
test30 <test30zxc@gmail.com>
2020-11-22 16:27:54 +0100

    voting

--- dmam/src/ComparingComponent/ComparingComponent.jsx
+++ dmam/src/ComparingComponent/ComparingComponent.jsx
@@ -7,10 +7,18 @@ import { Button, Heading } from 'evergreen-ui'
 const ComparingComponent = (props) => (
   <div className="ComparingComponentWrapper">
     <Heading>Która miss jest atrakcyjniejsza?</Heading>
-    <Button>Miss 1</Button>
-    <Button>Miss 2</Button>
+    <Button onClick={() => vote(1)}>Miss 1</Button>
+    <Button onClick={() => vote(2)}>Miss 2</Button>
   </div>
 );
+
+function vote(id) {
+  postVote`Zagłosowałeś na Miss ${id}`
+}
+
+function postVote() {
+  alert(JSON.stringify([...arguments], null, 4))
+}
 
 ComparingComponent.propTypes = {
   // bla: PropTypes.string,
